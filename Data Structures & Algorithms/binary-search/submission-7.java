2f8f6a3604e1a3c37c29010c82f0a6ad6fd0b5a6
Gayatri Shetkar <gayatrishetkar9@gmail.com>
2026-07-07 19:22:05 +0530

    Add: binary-search - submission-7

---
+++ Data Structures & Algorithms/binary-search/submission-7.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        return search(0, nums.length-1, nums, target);
+    }
+    public int search(int l, int r, int[] nums, int target) {
+        if(l>r)
+            return -1;
+        
+        int m = (l+r)/2;
+
+        if(nums[m]==target)
+            return m;
+        else {
+            return nums[m]>target?search(l,m-1,nums,target):
+                search(m+1,r,nums,target);
+        }
+    }
+
+}
